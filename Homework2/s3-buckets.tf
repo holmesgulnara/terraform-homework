b15fb43dc4db1552d48c865d643ef6af30ce1c0a
holmesgulnara <holmesgulnara@gmail.com>
2024-04-06 12:32:29 +0000

    terrcreated key, users, groups, and s3 bucket using loop function

---
+++ Homework2/s3-buckets.tf
@@ -0,0 +1,24 @@
+resource "aws_s3_bucket" "hw2-s3" {
+  bucket = "kaizen-gulya"
+  force_destroy = true
+}
+
+resource "aws_s3_bucket" "hw2-s3_1" {
+  bucket_prefix = "kaizen-"
+  force_destroy = true
+}
+
+# importing the S3 buckets created manually in AWS Console:
+resource "aws_s3_bucket" "hw2-s3_2" {
+  bucket_prefix = "kaizen-gulya1"
+  force_destroy = true
+}
+
+resource "aws_s3_bucket" "hw2-s3_3" {
+  bucket_prefix = "kaizen-gulya2"
+  force_destroy = true
+}
+
+#commands I have run to import users created manually:
+#  terraform import aws_s3_bucket.hw2-s3_2 kaizen-gulya1
+#  terraform import aws_s3_bucket.hw2-s3_3 kaizen-gulya2
