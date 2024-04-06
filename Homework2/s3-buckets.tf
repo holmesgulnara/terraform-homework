8dde7321ab0ffdda13130c1befed7d760fe0717a
holmesgulnara <holmesgulnara@gmail.com>
2024-04-06 12:45:39 +0000

    Cleanup code

--- Homework2/s3-buckets.tf
+++ Homework2/s3-buckets.tf
@@ -8,7 +8,7 @@ resource "aws_s3_bucket" "hw2-s3_1" {
   force_destroy = true
 }
 
-# importing the S3 buckets created manually in AWS Console:
+# importing the S3 buckets created manually in AWS Console to Terraform:
 resource "aws_s3_bucket" "hw2-s3_2" {
   bucket_prefix = "kaizen-gulya1"
   force_destroy = true
@@ -19,6 +19,6 @@ resource "aws_s3_bucket" "hw2-s3_3" {
   force_destroy = true
 }
 
-#commands I have run to import users created manually:
+#commands to run in terminal to import S3 buckets created manually:
 #  terraform import aws_s3_bucket.hw2-s3_2 kaizen-gulya1
 #  terraform import aws_s3_bucket.hw2-s3_3 kaizen-gulya2
