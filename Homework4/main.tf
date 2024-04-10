1d7294b10198ab43f4252be4582d26d2861a953d
holmesgulnara <holmesgulnara@gmail.com>
2024-04-10 18:40:23 +0000

    Homework 4 task

---
+++ Homework4/main.tf
@@ -0,0 +1,52 @@
+variable region {
+    description = "Provide region"
+    default = ""
+    type = string
+}
+
+variable ami_id {
+    description = "Provide ami id"
+    default = ""
+    type = string
+}
+
+variable type {
+    description = "Provide instance type"
+    default = ""
+    type = string
+}
+
+variable availibility_zone {
+    description = "Provide availibility zone"
+    default = ""
+    type = string
+}
+
+variable key_name {
+    description = "Provide key name"
+    default = ""
+    type = string
+}
+
+# variable count {
+#     description = "Provide number of Instances"
+#     default = 1
+#     type = number
+# }
+
+variable "instances_per_subnet" {
+  description = "Number of EC2 instances in each private subnet"
+  type        = number
+  default     = 1
+}
+
+
+
+resource "aws_instance" "app" {
+  ami           = var.ami_id
+  instance_type = var.type
+  availability_zone = var.availibility_zone
+  vpc_security_group_ids = [aws_security_group.allow_tls.id]
+  key_name = var.key_name
+  count = var.instances_per_subnet
+}
